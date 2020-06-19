4744156fddf1d2037beae5c1b8396b945a168d98
adamaaronson <adamaaronson@gmail.com>
2020-06-18 21:46:20 -0500

    Updates

---
+++ precache-manifest.395ffe6ef7a2cb7f7fd528585bb5b3e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "334580930662cb5351965e59fc414b79",
+    "url": "/known-human-beings/index.html"
+  },
+  {
+    "revision": "5afedff96e268f8200e7",
+    "url": "/known-human-beings/static/css/main.f539c943.chunk.css"
+  },
+  {
+    "revision": "3580bf72802ed26095f1",
+    "url": "/known-human-beings/static/js/2.ee07e16d.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/known-human-beings/static/js/2.ee07e16d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5afedff96e268f8200e7",
+    "url": "/known-human-beings/static/js/main.2d3716f2.chunk.js"
+  },
+  {
+    "revision": "1b5f65f577369af089cf",
+    "url": "/known-human-beings/static/js/runtime-main.d855ec26.js"
+  }
+]);
