d7b709bd69fb66fc87ab0e56e5c16e3eb182bd04
adamaaronson <adamaaronson@gmail.com>
2020-06-18 21:56:33 -0500

    Don't all load at once

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            vowelFilter: '',
+            vowelFilter: 'A',
             humans: [],
             mentions: []
         }
