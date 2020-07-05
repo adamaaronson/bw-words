78ca6db8887c73c5b9d8f658b8e73e1a4a1c3d84
adamaaronson <adamaaronson@gmail.com>
2020-07-05 18:20:32 -0500

    Updates

---
+++ supervocalics/precache-manifest.cc423ccaf492cf599e84c4a1ac9cbbe4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7367c2eb9b1aa8e5b7970b5366c708dd",
+    "url": "/supervocalics/index.html"
+  },
+  {
+    "revision": "9eaf77002838b3de6156",
+    "url": "/supervocalics/static/css/main.399f564b.chunk.css"
+  },
+  {
+    "revision": "535897a1e3c9736af28c",
+    "url": "/supervocalics/static/js/2.f4562bb1.chunk.js"
+  },
+  {
+    "revision": "7edc9d6a5411dd85ffa426b7d3cb6e20",
+    "url": "/supervocalics/static/js/2.f4562bb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9eaf77002838b3de6156",
+    "url": "/supervocalics/static/js/main.a00d4020.chunk.js"
+  },
+  {
+    "revision": "04d49f28ebe18ae7cf21",
+    "url": "/supervocalics/static/js/runtime-main.afb72ddf.js"
+  }
+]);
